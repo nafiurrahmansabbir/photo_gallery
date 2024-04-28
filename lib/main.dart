a069a19d66c3e25adce8422c8b9ea8a953a74b0d
nafiurrahmansabbir <nafiurrahmansabbir@gmail.com>
2024-04-28 13:38:03 +0600

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:device_preview/device_preview.dart';
 void main() {
   runApp(
     DevicePreview(
-      enabled: true,
+      enabled: false,
       builder: (context) => const MyApp(),
     ),
   );
